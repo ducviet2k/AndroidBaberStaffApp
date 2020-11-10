3ce0771bf3e7e25f31f6f2f16ae413705623bb0f
thanhdat68 <datnguyendinh6886@gmail.com>
2020-11-10 10:12:21 +0700

    done part25

---
+++ app/src/main/java/com/example/androidbaberstaffapp/Interface/IBarberServiceLoadListener.java
@@ -0,0 +1,10 @@
+package com.example.androidbaberstaffapp.Interface;
+
+import com.example.androidbaberstaffapp.Model.BarberServices;
+
+import java.util.List;
+
+public interface IBarberServiceLoadListener {
+    void onBarberServicesLoadSuccess(List<BarberServices> barberServicesList);
+    void onBarberServicesLoadFail(String message);
+}
